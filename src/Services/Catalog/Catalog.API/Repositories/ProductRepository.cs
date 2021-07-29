0330a146c0fce7090d909cbb7d8475298536cdde
Prem <prem.keshari@sapient.com>
2021-07-30 00:13:23 +0530

    Commit catalog.api project

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,58 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        private readonly ICatalogContext _context;
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+             var DeleteResult = await _context.Products.DeleteOneAsync(g => g.Id== id);
+            return DeleteResult.IsAcknowledged && DeleteResult.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context.Products.Find(p => p.Id==id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string name)
+        {
+            return await _context.Products.Find(p => p.Category==name).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            var data= await _context.Products.Find(p => true).ToListAsync();
+            return data;
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _context.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -10,6 +10,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 
 namespace Catalog.API
 {
@@ -31,6 +35,8 @@ namespace Catalog.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
             });
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
